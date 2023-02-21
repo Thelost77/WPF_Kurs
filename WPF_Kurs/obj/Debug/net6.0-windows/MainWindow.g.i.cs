d315bc710ccf578ba21031f5111f8e17d9455573
wiktor.ziebka <wiktor.ziebka@externals.hilti.com>
2023-02-21 12:59:35 +0100

    update

--- WPF_Kurs/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ WPF_Kurs/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -213,7 +213,7 @@ namespace Wpf_Kurs {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Wpf_Kurs;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Calculator;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
